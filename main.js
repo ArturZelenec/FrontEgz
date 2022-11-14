0e98e6c8a0b5e40c2f8ec02d8fa1ca19d0099df9
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-11-14 19:44:54 +0200

    login

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ const setButtons = () => {
     registerBtn.addEventListener('click', () => {
         registerBox.style.display = 'block';
         loginBox.style.display = 'none';
-        
+
     });
 
     loginBtn.addEventListener('click', () => {
         registerBox.style.display = 'none';
         loginBox.style.display = 'block';
-        
+
     });
 }
 
@@ -36,7 +36,7 @@ function sendData() {
 
 
     data.forEach((value, key) => {
-        
+
         obj[key] = value
     });
     console.log(obj);
@@ -47,17 +47,79 @@ function sendData() {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        
-        body: JSON.stringify(obj) 
+
+        body: JSON.stringify(obj)
     })
-    .then(obj => console.log(obj.json()))
-    .catch((eror) => console.log(eror));
+        .then(obj => console.log(obj.json()))
+        .catch((eror) => console.log(eror));
 }
 
 registerFormSbmBtn.addEventListener('click', (e) => {
     e.preventDefault(); // Breaks manual refresh after submit
     sendData();
 })
+
+
+
+
+
+
+
+
+
+
+
+const logFirstName = document.querySelector('#name-inp-log');
+const logLastName = document.querySelector('#surname-inp-log');
+const loginFormSbmBtn = document.querySelector('#login-form-submit');
+const url = 'https://testapi.io/api/azelenec/resource/reg';
+
+const options = {
+    method: 'get',
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    }
+}
+
+function patikrintiVartotojus() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((userData) => {
+            let arEgzistuoja = false;
+            userData.data.forEach(user => {
+                if (user.userName.toLowerCase() === logFirstName.value.toLowerCase() && user.surName.toLowerCase() === logLastName.value.toLowerCase()) {
+                    console.log(`vardas ${user.userName} ir pavarde ${user.surName} ir email ${user.email}`);
+                    window.alert(`Sveiki prisijunge ${user.userName} `);
+                    arEgzistuoja = true
+                    const vartotojoDuomenys = {
+
+                        regUserName: user.userName,
+                        regUserLastname: user.surName,
+
+                    };
+
+                }
+
+            });
+
+            if (!arEgzistuoja)
+                window.alert(`Toks vartotojas Neegzistuoja `);
+
+
+
+        })
+        .catch((eror) => console.log(eror));
+}
+
+
+loginFormSbmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    patikrintiVartotojus();
+});
+
+
+
 
 
 
